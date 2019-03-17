94c6a01d10e416c8629acce6dc7c6b6fa6e22f2b
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-03-17 13:34:25 +0000

    Created class-based app component.

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
+  state = { resource: 'posts' }
+
   render() {
     return (
       <div className="ui container">
-        App
+        <div>
+          <button className="ui button primary" onClick={() => this.setState({ resource: 'posts' })}>Posts</button>
+          <button className="ui button secondary" onClick={() => this.setState({ resource: 'todos' })}>Todos</button>
+        </div>
+        {this.state.resource}
       </div>
     );
   }
